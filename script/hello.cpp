2edcedffede0742e3193a5934fdf97f16b7ad3d9
robond <g@georgiechallis.com>
2020-05-20 15:34:17 -0700

    feat: Add Gazebo screenshot

--- script/hello.cpp
+++ script/hello.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginMyRobot() : WorldPlugin()
             {
-              printf("Welcome to Georgie's World!'\n");
+              printf("Welcome to my World!'\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
